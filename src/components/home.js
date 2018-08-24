59ff72210c38f1fb3a7cfd696498fe9862c10e03
Hayden Bruin <bruinhayden@gmail.com>
2018-08-25 00:53:09 +1200

    Setting up icons & hero functionality

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,7 @@
 import React, { Fragment, Component } from 'react';
 
+import Icon from "./utils/icons";
+
 class Home extends Component {
 
     state = {
@@ -9,34 +11,39 @@ class Home extends Component {
             "Javascript",
             "React",
             "Redux",
-            "ES6"
+            "ES6",
+            "VueJS"
         ],
-        workwithcounter: 0,
-        workwithstring: ""
+        workwithcounter: 0
     }
 
     componentDidMount(){
         setInterval(() => {
-            var word = this.state.workwith[this.state.workwithcounter].split("");
-            setInterval(() => {
-
-            })
-            // UPDATE WORK TYPE COUNT
             const count = (
                 (this.state.workwithcounter + 1) >= this.state.workwith.length ?
                 0 : this.state.workwithcounter + 1
             )
             this.setState({ workwithcounter: count })
-
         }, 2000);
+    }
+
+    scrollDown = () => {
+        console.log('scroll down');
     }
 
     render() {
         return (
             <Fragment>
                 <div className="hero">
-                    <h1>Hi, I'm Hayden Bruin</h1>
-                    <h2>I work with <span>{this.state.workwith[this.state.workwithcounter]}</span></h2>
+                    <div className="hero__content">
+                        <h1>Hi, I'm Hayden Bruin</h1>
+                        <h2>I <Icon name="heart" height="24" width="24" /> <span>{this.state.workwith[this.state.workwithcounter]}</span></h2>
+                    </div>
+                    <div className="hero__footer">
+                        <div onClick={() => this.scrollDown()}>
+                            <Icon name="arrow-down" width="48px" height="48px" />
+                        </div>
+                    </div>
                 </div>
             </Fragment>
         );
